979af453ce2a8be982e0f35c5789a1a813235827
luiscascelli <luis.cascelli@gmail.com>
2021-02-22 14:49:33 -0300

    criada classe PessoaL

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/PessoaL.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+class PessoaL {
+    var nome: String = "Luis"
+    var cpf: String = "08585301635"
+}
+
+fun main(){
+    val luis = PessoaL()
+    println(luis.nome)
+    println(luis.cpf)
+}
